fea7457edd06f3c554f3a82dbb8fb6f7dbab9e7e
DESTERIO-RASMENTO <desochudi@gmail.com>
2021-08-27 17:28:53 +0300

    designs

--- app/src/main/java/com/example/hsmicrofinance/Fragments/BasicPayLoans.java
+++ app/src/main/java/com/example/hsmicrofinance/Fragments/BasicPayLoans.java
@@ -2,11 +2,14 @@ package com.example.hsmicrofinance.Fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.example.hsmicrofinance.R;
 import com.example.hsmicrofinance.databinding.FragmentBasicPayLoansBinding;
@@ -29,5 +32,23 @@ public class BasicPayLoans extends Fragment {
         // Inflate the layout for this fragment
         mFragmentBasicPayLoansBinding = FragmentBasicPayLoansBinding.inflate(inflater,container,false);
         return mFragmentBasicPayLoansBinding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mFragmentBasicPayLoansBinding.payloanSubmitBtn.setOnClickListener(v-> executePayloan());
+    }
+
+    private void executePayloan() {
+        if(mFragmentBasicPayLoansBinding.payloanAccountNo.getText().toString().trim().isEmpty() || mFragmentBasicPayLoansBinding.payloanAmount.getText().toString().trim().isEmpty()){
+            mFragmentBasicPayLoansBinding.payloanErrorTxt.setText("All fields required");
+        }else {
+            doPayLoan();
+        }
+    }
+
+    private void doPayLoan() {
+        Toast.makeText(getContext(), "Pay loans", Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/com/example/hsmicrofinance/Fragments/BasicPayfriends.java
+++ app/src/main/java/com/example/hsmicrofinance/Fragments/BasicPayfriends.java
@@ -2,11 +2,14 @@ package com.example.hsmicrofinance.Fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.example.hsmicrofinance.R;
 import com.example.hsmicrofinance.databinding.FragmentBasicPayfriendsBinding;
@@ -28,5 +31,23 @@ public class BasicPayfriends extends Fragment {
         // Inflate the layout for this fragment
         mFragmentBasicPayfriendsBinding = FragmentBasicPayfriendsBinding.inflate(inflater,container,false);
         return mFragmentBasicPayfriendsBinding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mFragmentBasicPayfriendsBinding.payfriendSubmitBtn.setOnClickListener(v-> executepayfriend());
+    }
+
+    private void executepayfriend() {
+        if(mFragmentBasicPayfriendsBinding.payfriendAccountNo.getText().toString().trim().isEmpty() || mFragmentBasicPayfriendsBinding.payfriendAmount.getText().toString().trim().isEmpty()){
+            mFragmentBasicPayfriendsBinding.payfriendErrorTxt.setText("All fields required");
+        }else{
+            doPayFriends();
+        }
+    }
+
+    private void doPayFriends() {
+        Toast.makeText(getContext(), "PayFriends", Toast.LENGTH_SHORT).show();
     }
 }
