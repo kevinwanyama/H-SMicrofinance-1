d3c0203d9ee0f0d2e5bfc4d27a49a0c1548e49f3
DESTERIO-RASMENTO <desochudi@gmail.com>
2021-08-31 13:25:47 +0300

    testst pull

--- app/src/main/java/com/example/hsmicrofinance/Fragments/BasicEDeposit.java
+++ app/src/main/java/com/example/hsmicrofinance/Fragments/BasicEDeposit.java
@@ -2,6 +2,8 @@ package com.example.hsmicrofinance.Fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -14,6 +16,7 @@ import com.example.hsmicrofinance.databinding.FragmentBasicEDepositBinding;
 
 public class BasicEDeposit extends Fragment {
 
+    private static final String TAG = "BasicEdeposit";
 
 FragmentBasicEDepositBinding mFragmentBasicEDepositBinding;
     public BasicEDeposit() {
@@ -28,5 +31,10 @@ FragmentBasicEDepositBinding mFragmentBasicEDepositBinding;
         // Inflate the layout for this fragment
         mFragmentBasicEDepositBinding = FragmentBasicEDepositBinding.inflate(inflater,container,false);
         return mFragmentBasicEDepositBinding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
     }
 }
